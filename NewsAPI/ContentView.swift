b08a8ce1bce709772bd317ec5f222ae7d7404a40
Finn <FinnOlshan@gmail.com>
2024-12-10 14:45:38 -0500

    happy world

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("goodbye world!")
+            Text("goodbye happy world!")
         }
         .padding()
     }
